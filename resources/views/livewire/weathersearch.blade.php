6bf14f3385249a5e06e8c8bb05391c7f0969955a
Rico De Santis <ricodesantis@gmail.com>
2025-05-04 21:22:05 -0400

    improve button accessibility
    Add aria-label to form button for improved accessibility

--- resources/views/livewire/weathersearch.blade.php
+++ resources/views/livewire/weathersearch.blade.php
@@ -20,7 +20,7 @@
         </div>
     </div>
     <div class="flex justify-center mt-4">
-        <button wire:click="searchWeather" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Search</button>
+        <button wire:click="searchWeather" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" aria-label="Submit form">Search</button>
     </div>
     @if($error)
         <p class="text-center">{{$error}}</p>
